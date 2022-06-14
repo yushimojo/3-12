33fb8ae1260576d98f6c74dd1a11f63cb5d9567a
YuShimojo <yongxiatiao@gmail.com>
2022-06-14 14:04:50 +0000

    [add]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
   
    def index
+       @book=Book.new
        @books=Book.all
    end
   
   def create 
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)  
+    @book=Book.new(book_params)
+  if @book.save
+    redirect_to book_path(@book.id)  
+  else
+      @books=Book.all
+    render:index 
+  end 
+  end
+  
+  def create 
+   @book = Book.new(book_params)
+   if @book.save
+       flash[:notice] = "successfully"
+       redirect_to book_path
+   end 
   end 
 
   def show
@@ -16,11 +29,31 @@ class BooksController < ApplicationController
 
   def edit
       @book = Book.find(params[:id])
+      @books=Book.all
       
+ 
+  end 
       
+  def update
+    @book = Book.find(params[:id])
+   if @book.save
+    redirect_to edit_book_path(@book.id)  
+   else
+    render:edit
+   end
   end
+  
+  def destroy
+    book = Book.find(params[:id])  
+    book.destroy
+    redirect_to books_path 
+  end 
 
-private
+      
+      
+
+
+ private
  def book_params
   params.require(:book).permit(:title, :body)
  end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   post 'books'=>'books#create'
   get 'books/:id' =>'books#show',as:'book'
   get 'books/:id/edit'=>'books#edit',as:'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
